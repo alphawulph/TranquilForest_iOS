ad989be2ca35593cba2fd872b41e626fb64ca443
Dave R <alphawulph@gmail.com>
2020-06-24 10:55:20 -0500

    .

--- TranquilForest/DonateViewController.swift
+++ TranquilForest/DonateViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import StoreKit
 
-class DonateViewController: UIViewController {
-
+class DonateViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SKProductsRequestDelegate {
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        <#code#>
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    var tableView = UITableView()
+    let productIdentifiers = Set([""])
+    var product: SKProduct?
+    var productsArray = Array(arrayLiteral: String())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +46,51 @@ class DonateViewController: UIViewController {
             } else {
                 UIApplication.shared.openURL(requestUrl)
             }
+        }
+    }
+    
+    func requestProductData() {
+        if SKPaymentQueue.canMakePayments() {
+            let request = SKProductsRequest(productIdentifiers: productIdentifiers)
+            request.delegate = self
+            request.start()
+        } else {
+            let alert = UIAlertController(title: "In-App Purchases Not Enabled", message: "Please enable In App Purchase in Settings", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Settings", style: UIAlertAction.Style.default, handler: { alertAction in
+                alert.dismiss(animated: true, completion: nil)
+                
+                let url: NSURL? = NSURL(string: UIApplication.openSettingsURLString)
+                if url != nil
+                {
+                    UIApplication.shared.open(url! as URL)
+                }
+                
+            }))
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { alertAction in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func productsRequest(request: SKProductsRequest!, didReceiveResponse response:  SKProductsResponse!) {
+       
+        let products = response.products
+        
+        if (products.count != 0) {
+            for product in products
+            {
+                self.product = product
+                self.productsArray.append(product.localizedTitle)
+            }
+            self.tableView.reloadData()
+        } else {
+            print("No products found")
+        }
+        
+        for product in response.invalidProductIdentifiers
+        {
+            print("Product not found: \(product)")
         }
     }
 }
